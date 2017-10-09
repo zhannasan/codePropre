55e1c143eb5bc8f6a663c1a6150955e332cf35c6
ricbon35 <ricbon35@gmail.com>
2017-10-09 12:59:10 +0200

    Init ex5

---
+++ src/main/java/ex5/Pret.java
@@ -0,0 +1,125 @@
+package ex5;
+
+import java.util.Date;
+
+public class Pret {
+
+	private Strategy strategy;
+	private double capital;
+	private double capitalRestantDu;
+	private double taux;
+	private Date dateDebut;
+	private Date dateFin;
+	
+	public Pret(Strategy strategy, double capital, double taux, Date dateDebut) {
+		super();
+		this.strategy = new PretATerme();
+		this.capital = capital;
+		this.taux = taux;
+		this.dateDebut = dateDebut;
+	}
+	
+	public Pret(Strategy strategy, double capital, double taux, Date dateDebut, Date dateFin) {
+		super();
+		this.strategy = new PretATerme();
+		this.capital = capital;
+		this.taux = taux;
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+	}
+	
+	public Pret(Strategy strategy, double capital, double capitalRestantDu, double taux, Date dateDebut, Date dateFin) {
+		super();
+		this.strategy = new PretATerme();
+		this.capital = capital;
+		this.capitalRestantDu = capitalRestantDu;
+		this.taux = taux;
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+	}
+
+	/** Getter for strategy
+	 * @return the strategy
+	 */
+	public Strategy getStrategy() {
+		return strategy;
+	}
+
+	/** Setter
+	 * @param strategy the strategy to set
+	 */
+	public void setStrategy(Strategy strategy) {
+		this.strategy = strategy;
+	}
+
+	/** Getter for capital
+	 * @return the capital
+	 */
+	public double getCapital() {
+		return capital;
+	}
+
+	/** Setter
+	 * @param capital the capital to set
+	 */
+	public void setCapital(double capital) {
+		this.capital = capital;
+	}
+
+	/** Getter for capitalRestantDu
+	 * @return the capitalRestantDu
+	 */
+	public double getCapitalRestantDu() {
+		return capitalRestantDu;
+	}
+
+	/** Setter
+	 * @param capitalRestantDu the capitalRestantDu to set
+	 */
+	public void setCapitalRestantDu(double capitalRestantDu) {
+		this.capitalRestantDu = capitalRestantDu;
+	}
+
+	/** Getter for taux
+	 * @return the taux
+	 */
+	public double getTaux() {
+		return taux;
+	}
+
+	/** Setter
+	 * @param taux the taux to set
+	 */
+	public void setTaux(double taux) {
+		this.taux = taux;
+	}
+
+	/** Getter for dateDebut
+	 * @return the dateDebut
+	 */
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	/** Setter
+	 * @param dateDebut the dateDebut to set
+	 */
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	/** Getter for dateFin
+	 * @return the dateFin
+	 */
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	/** Setter
+	 * @param dateFin the dateFin to set
+	 */
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	
+}
---
+++ src/main/java/ex5/PretATerme.java
@@ -0,0 +1,5 @@
+package ex5;
+
+public class PretATerme implements Strategy {
+
+}

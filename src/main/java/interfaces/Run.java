6efd3785f051abd8cd4e743ab2bb01f475e4ca81
ricbon35 <ricbon35@gmail.com>
2019-05-07 14:36:36 +0200

    corrigé

--- src/main/java/interfaces/Run.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Run {
-
-	public default int getSpeed() {
-		// TODO Auto-generated method stub
-		return 10;
-	}
-}
--- src/main/java/interfaces/Walk.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Walk {
-
-	public default int getSpeed() {
-		// TODO Auto-generated method stub
-		return 20;
-	}
-}

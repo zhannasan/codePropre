318c451c319110130eafa85da0e92d84976f7d55
Zhanna <j.santybay@gmail.com>
2019-12-14 06:04:08 +0100

    ex4 Pret

--- src/main/java/ex4/Pret.java
+++ src/main/java/ex4/Pret.java
@@ -2,93 +2,21 @@ package ex4;
 
 import java.util.Date;
 
-public class Pret {
-
-	private Strategy strategy;
-	private double capital;
-	private double capitalRestantDu;
-	private double taux;
+public class Pret extends PretATerme{
 	private Date dateDebut;
 	private Date dateFin;
 	
-	public Pret(double capital, double taux, Date dateDebut) {
-		super();
-		this.capital = capital;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
-	}
-	
-	public Pret(double capital, double taux, Date dateDebut, Date dateFin) {
-		super();
-		this.capital = capital;
-		this.taux = taux;
-		this.dateDebut = dateDebut;
-		this.dateFin = dateFin;
-	}
-	
+	/**
+	 * @param capital
+	 * @param capitalRestantDu
+	 * @param taux
+	 * @param dateDebut
+	 * @param dateFin
+	 */
 	public Pret(double capital, double capitalRestantDu, double taux, Date dateDebut, Date dateFin) {
-		super();
-		this.capital = capital;
-		this.capitalRestantDu = capitalRestantDu;
-		this.taux = taux;
+		super(capital, capitalRestantDu, taux);
 		this.dateDebut = dateDebut;
 		this.dateFin = dateFin;
-	}
-
-	/** Getter for strategy
-	 * @return the strategy
-	 */
-	public Strategy getStrategy() {
-		return strategy;
-	}
-
-	/** Setter
-	 * @param strategy the strategy to set
-	 */
-	public void setStrategy(Strategy strategy) {
-		this.strategy = strategy;
-	}
-
-	/** Getter for capital
-	 * @return the capital
-	 */
-	public double getCapital() {
-		return capital;
-	}
-
-	/** Setter
-	 * @param capital the capital to set
-	 */
-	public void setCapital(double capital) {
-		this.capital = capital;
-	}
-
-	/** Getter for capitalRestantDu
-	 * @return the capitalRestantDu
-	 */
-	public double getCapitalRestantDu() {
-		return capitalRestantDu;
-	}
-
-	/** Setter
-	 * @param capitalRestantDu the capitalRestantDu to set
-	 */
-	public void setCapitalRestantDu(double capitalRestantDu) {
-		this.capitalRestantDu = capitalRestantDu;
-	}
-
-	/** Getter for taux
-	 * @return the taux
-	 */
-	public double getTaux() {
-		return taux;
-	}
-
-	/** Setter
-	 * @param taux the taux to set
-	 */
-	public void setTaux(double taux) {
-		this.taux = taux;
 	}
 
 	/** Getter for dateDebut
--- src/main/java/ex4/PretATerme.java
+++ src/main/java/ex4/PretATerme.java
@@ -1,5 +1,49 @@
 package ex4;
 
 public class PretATerme implements Strategy {
+	private double capital;
+	private double capitalRestantDu;
+	private double taux;
+	private Strategy strategy;
+	/**
+	 * @param capital
+	 * @param capitalRestantDu
+	 * @param taux
+	 */
+	public PretATerme(double capital, double capitalRestantDu, double taux) {
+		this.capital = capital;
+		this.capitalRestantDu = capitalRestantDu;
+		this.taux = taux;
+	}
+	/**
+	 * @return the capital
+	 */
+	public double getCapital() {
+		return capital;
+	}
+	/**
+	 * @return the capitalRestantDu
+	 */
+	public double getCapitalRestantDu() {
+		return capitalRestantDu;
+	}
+	/**
+	 * @return the taux
+	 */
+	public double getTaux() {
+		return taux;
+	}
+	/** Getter for strategy
+	 * @return the strategy
+	 */
+	public Strategy getStrategy() {
+		return strategy;
+	}
 
+	/** Setter
+	 * @param strategy the strategy to set
+	 */
+	public void setStrategy(Strategy strategy) {
+		this.strategy = strategy;
+	}
 }
